ef09e3858fe16ab19d82771a0d9bd75ea4a102fa
peter-keller <keller.f.peter@gmail.com>
2018-05-11 20:33:05 +0100

    api keys removed

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const body = document.querySelector("body");
 const buttons = document.getElementsByClassName("dayButton");
 
 
-const ipStackKey = "5bbd3d01ffb7673da9dcd7dc22d800f3";
-const accuWeatherKey = "9AI2zAYeN5HsQmwlLyHxvtS4RPTOK1Cb";
+const ipStackKey = "";
+const accuWeatherKey = "";
 
 
 const coordinates = new XMLHttpRequest();
