5c0fceb1d45951b3630f6828580985d7305ba7e7
peter-keller <keller.f.peter@gmail.com>
2018-05-11 20:28:27 +0100

    feat(index.js): gps location from IP address

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 const body = document.querySelector("body");
 const buttons = document.getElementsByClassName("dayButton");
@@ -6,3 +6,10 @@ const buttons = document.getElementsByClassName("dayButton");
 
 const ipStackKey = "5bbd3d01ffb7673da9dcd7dc22d800f3";
 const accuWeatherKey = "9AI2zAYeN5HsQmwlLyHxvtS4RPTOK1Cb";
+
+
+const coordinates = new XMLHttpRequest();
+coordinates.open('GET', "http://api.ipstack.com/check?access_key=" + ipStackKey, false);
+coordinates.send();
+let ipStackResponse = JSON.parse(coordinates.response);
+let locationCoordinates = ipStackResponse.latitude + "%2C" + ipStackResponse.longitude;
