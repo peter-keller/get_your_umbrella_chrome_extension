925faee0d0799ae17ec028da3bea13c761baf842
peter-keller <keller.f.peter@gmail.com>
2018-05-11 20:30:01 +0100

    feat(index.js): weather in given city

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ const coordinates = new XMLHttpRequest();
 coordinates.open('GET', "http://api.ipstack.com/check?access_key=" + ipStackKey, false);
 coordinates.send();
 let ipStackResponse = JSON.parse(coordinates.response);
+
 let locationCoordinates = ipStackResponse.latitude + "%2C" + ipStackResponse.longitude;
 
 
 const getCity = new XMLHttpRequest();
 getCity.open('GET', "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=" + accuWeatherKey + "&q=" + locationCoordinates + "&details=true&toplevel=true", false);
 getCity.send();
-let cityResponse = JSON.parse(getCity.response)
+let cityResponse = JSON.parse(getCity.response);
+
+
+const weather = new XMLHttpRequest();
+weather.open("GET", "http://dataservice.accuweather.com/forecasts/v1/daily/5day/" + cityResponse.Key + "?apikey=" + accuWeatherKey + "&details=true&metric=true ", false);
+weather.send(null);
+let weatherResponse = JSON.parse(weather.response);
